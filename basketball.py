bb9a27b22ff3934fb0362ef0dbfc4d77ec82d8e9
jwatts41 <wattsjonovan@yahoo.com>
2025-03-05 21:32:27 -0500

    added in more questions and added in numbers to each question and how many remain

--- basketball.py
+++ basketball.py
@@ -8,44 +8,52 @@ if playing.lower() != "yes":
 print("Let's get started")
 score = 0
 
-question = input("Which NBA team has the most championships? ")
+question = input("Question 1. Which NBA team has the most championships? ")
 if question.lower() == "lakers" or question.lower() == "la lakers" or question.lower() == "los angeles lakers":
-    print("Correct!")
+    print("Correct! 6 Questions Remaining")
     score += 1
 else:
-    print("Incorrect. The correct answer is the Lakers.")
+    print("Incorrect. The correct answer is the Lakers. 6 Questions Remaining")
 
-question = input("Which Collge Basketball team has the most NCAA championships? ")
+question = input("Question 2. Which Collge Basketball team has the most NCAA championships? ")
 if question.lower() == "ucla":
-    print("Correct!")
+    print("Correct! 5 Questions Remaining")
     score += 1
 else:
-    print("Incorrect. The correct answer is UCLA.")
+    print("Incorrect. The correct answer is UCLA. 5 Questions Remaining")
 
-question = input("Which NBA team holds the record for the most regular season wins? ")
+question = input("Question 3. Which NBA team holds the record for the most regular season wins? ")
 if question.lower() == 'warriors' or question.lower() == "golden state" or question.lower() == "golden state warriors":
-    print("Correct")
+    print("Correct! 4 Questions Remaining")
     score += 1
 else:
-    print("Incorrect. The correct answer is the Golden State Warriors.")
+    print("Incorrect. The correct answer is the Golden State Warriors. 4 Questions Remaining")
 
-question = input("How many teams are currently in the NBA? ")
+question = input("Question 4. How many teams are currently in the NBA? ")
 if question == "30":
-    print("Correct!")
+    print("Correct! 3 Questions Remaining")
     score += 1
 else:
-    print("Incorrect, the correct answer is 30.")
+    print("Incorrect, the correct answer is 30. 3 Questions Remaining")
 
-question = input("What college basketball coach holds the record for the most wins of all time? ")
+question = input("Question 5. What college basketball coach holds the record for the most wins of all time? ")
 if question.lower() == "coach k" or question.title() == "Coach K" or question.title() == "Mike Krzyzewski" or question.title() == "Krzyzewski":
-    print("Correct!")
+    print("Correct! 2 Questions Remaining")
     score += 1
 else:
-    print("Incorrect. The correct answer is Coach K.")
+    print("Incorrect. The correct answer is Coach K. 2 Questioning Remaining")
 
-question = input("What NBA basketball coach holds the record for the most wins of all time? ")
+question = input("Question 6. What NBA basketball coach holds the record for the most wins of all time? ")
 if question.lower() == "gregg popovich" or question.title() == "Gregg Popovich" or question.title() == "Popovich":
-    print("Correct!")
+    print("Correct! 1 Question Remaining")
     score +=1
 else:
-    print("Incorrect. The correct answer is Gregg Popovich.")
+    print("Incorrect. The correct answer is Gregg Popovich. 1 question remaining")
+
+question = input("Question 7. Who is a better college basketball team? North Carolina or Duke?")
+if question.lower() == "north carolina" or question.lower() == "carolina" or question.title() == "North Carolina":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect. The correct answer is North Carolina. Do better")
+
