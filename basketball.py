8919618a40d66e3e8f2eedde597d20aa17f8b5d3
jwatts41 <wattsjonovan@yahoo.com>
2025-03-05 20:58:22 -0500

    added and tested first 2 questions

--- basketball.py
+++ basketball.py
@@ -1,1 +1,23 @@
 print('Welcome to my basketball trivia quiz!')
+
+playing = input("You think you know basketball? If so, do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Let's get started")
+score = 0
+
+question = input("Which NBA team has the most championships? ")
+if question.lower() == "lakers" or question.lower() == "la lakers" or question.lower() == "los angeles lakers":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect. The correct answer is the Lakers.")
+
+question = input("Which Collge Basketball team has the most NCAA championships? ")
+if question.lower() == "ucla":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect. The correct answer is UCLA.")
